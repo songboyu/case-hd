ee63857a4abcf98bb28295848e1dc78a924e7e2f
scrawlor <scrawlormain@gmail.com>
2013-01-01 11:15:51 +0800

    update for Diagnostic report,mage list,sharing.
    image list can roll;
    Diagnostic report layout can move Dynamically;
    A number of departments can share the one,just having a chioce when you
    sign in.

---
+++ Case05_4/Case05_4/Case05_4/tableView.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Case05_4
+{
+    public partial class tableView : Form
+    {
+        List<string> p = new List<string>();
+        public tableView(BindingSource s)
+        {
+            InitializeComponent();
+
+            DataRowView SelectedRowView = (DataRowView)s.Current;
+            病历信息DataSet.infoRow selectedRow = (病历信息DataSet.infoRow)SelectedRowView.Row;
+            try
+            {
+                p.Add(selectedRow.table1);
+                p.Add(selectedRow.table2);
+                p.Add(selectedRow.table3);
+                p.Add(selectedRow.table4);
+                p.Add(selectedRow.table5);
+                p.Add(selectedRow.table6);
+                p.Add(selectedRow.table7);
+                p.Add(selectedRow.table8);
+                p.Add(selectedRow.table9);
+                p.Add(selectedRow.table10);
+            }
+            catch (Exception e) { }
+
+            for (int i = 0; i < selectedRow.tableNum; i++)
+            {
+                imageList1.Images.Add(Image.FromFile(p[i]));
+
+                int sign1 = p[i].LastIndexOf("\\");
+                int sign2 = p[i].LastIndexOf(".");
+                String name = p[i].Substring(sign1+1,sign2-sign1-1);
+
+                ListViewItem lvi = new ListViewItem();
+                lvi.ImageIndex = i;
+                lvi.Text = name;
+                lvi.ToolTipText = name;
+                this.listView1.Items.Add(lvi);
+
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            int i = listView1.SelectedItems[0].Index;
+            table form = new table(p[i]);
+            form.Show();
+        }
+
+    }
+}
---
+++ Case05_4/Case05_4/WindowsFormsControlLibrary1/IControlsMove.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace WindowsFormsControlLibrary1
+{
+    /// <summary>
+    /// 用于容器控件，使该容器内的控件可被用户移动和改变大小
+    /// </summary>
+    public interface IControlsMove
+    {
+       //  //enum EnumMousePointPosition{}
+       //void MyMouseDown(object sender, System.Windows.Forms.MouseEventArgs e);
+       //void MyMouseLeave(object sender, EventArgs e);
+       //  //EnumMousePointPosition MousePointPosition(Size size, System.Windows.Forms.MouseEventArgs e) { }
+       //void MyMouseMove(object sender, System.Windows.Forms.MouseEventArgs e);
+       // XmlDocument doc { set; get; }
+        /// <summary>
+        /// 初始化容器内鼠标事件和加载指定xml文档内的控件外观属性
+        /// </summary>
+        /// <param name="XmlDoc">存储该容器内的控件的大小和位置属性的XML文件</param>
+       void InitMouseAndContolStyle(XmlDocument XmlDoc);
+    }
+}
